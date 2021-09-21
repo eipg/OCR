13d09420b297d110a3140f94def1269490709931
彭国锋 <pengguofeng@pengguofengdeMacBook-Air.local>
2021-09-21 12:28:43 +0100

    update corss-vaild

--- ocr_rscript.R
+++ ocr_rscript.R
@@ -1,9 +1,24 @@
 library(kernlab)
-#divided into training set & test set
+library(e1071)
+library(caret)
+#Split the data 50% training, 25% validation and 25% test
 letters <- read.table("letterdata.txt", header=TRUE)
 letters$y <- as.factor(letters$y)
-training_set <- letters[1:16000, ]
-test_set <- letters[16001:20000, ]
+training_set <- letters[1:10000, ]
+valid_set <- letters[10001:15000,]
+test_set <- letters[15001:20000, ]
+
+
+#Fit the classification SVM for different values of C
+#and calculate the validation prediction error
+#divided into training set & test set
+C.sel<- tune.svm(y~.,data=training_set,type="C-classification",kernel="linear",
+                 cost=c(0.1,0.5,1,2,5,10))$best.parameters
+final.svm<-svm(y~.,data=training_set,kernel="linear",cost=C.sel,
+               type="C-classification",decision.values=T)
+fitted.train<-attributes(predict(final.svm,training_set,
+                                 decision.values = TRUE))$decision.values
+C.sel
 
 #linear model
 letter_classifier_linear <- ksvm(y ~., data = training_set, kernel="vanilladot")
@@ -40,3 +55,15 @@ table(agreement)
 prop.table(table(agreement))
 
 #plot
+
+#Plot the ROC for the validation data
+library(ROCR)
+pred <- prediction(fitted.valid,valid.data$sp,label.ordering=c("O","B"))
+#label.ordering: change the default ordering of the classes
+#by supplying a vector containing the negative and the positive class label
+perf <- performance(pred, "tpr", "fpr")
+plot(perf, main="Validation data ROC", cex.main=0.75, cex.lab=0.75)
+
+#Try different alternative classification methods, like decision trees, random forests,
+#neural networks and Gaussian processes, and compare their test set performance with that 
+#obtained with SVNs.
